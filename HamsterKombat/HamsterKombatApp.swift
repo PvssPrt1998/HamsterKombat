aa953ca3bfb632643292008daa8bb288f70bdbe0
WebShell1998 <43918790+WebShell1998@users.noreply.github.com>
2024-08-06 21:55:35 +0500

    Initial Commit

---
+++ HamsterKombat/HamsterKombatApp.swift
@@ -0,0 +1,17 @@
+//
+//  HamsterKombatApp.swift
+//  HamsterKombat
+//
+//  Created by Николай Щербаков on 06.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct HamsterKombatApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
